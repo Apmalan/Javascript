07912b5753883f6dab9802628923b2366a05e16a
Alan Monte <53954290+Apmalan@users.noreply.github.com>
2019-10-14 21:55:17 -0300

    Aula javaScript2

---
+++ assets/js/interacao.js
@@ -0,0 +1,14 @@
+var pessoa = {
+    Crianca:0,
+    Pre_adolescente: 0,
+    Adolescente: 0,
+    Adulto: 0,
+    Idoso:0,
+
+    },
+
+
+for(let index = 0; index < 10; index++) {
+    const element = Array[index];
+    
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@ var carro = {
 console.log(carro);
 console.log(pessoa)*/
 //#region Objeto Pessoa
-var pessoa = {
+/*var pessoa = {
     Nome: ['Welton', 'Castoldi'],
     Idade: 32,
     Sexo: 'masculino',
@@ -72,7 +72,156 @@ var pessoa = {
 
 /*function camelCase(){ /*  não sei o que é, pesquisar.*/
 
- console.log(pessoa)
+ /*console.log(pessoa)
  alert(pessoa.Sexo)
     
-    //#endregion
+    //#endregion*/
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //ESTRUTURA CONDICIONAIS OU DECISÃO OU SELEÇÃO
+    //Se seNao seNao se = Portugol
+    //Escolha/Caso = Portugol
+
+    //If Else Else If = JavaScript
+    //Switch / Case = JavaScript
+
+    //Verificar se é menor de idade..
+    /*var idade = 21;
+
+
+    if (idade <= 18) {
+        console.log("Menor de idade");
+
+       
+        } 
+        else{
+            console.log("Maior de idade");
+        }
+        
+        var dia = 2;
+        function diaSemana (valor);{
+            switch (valor) {
+                case 1:{
+                    console.log("Segunda");
+                    break;
+                }
+                case 2:{
+                    console.log("Segunda");
+                    break;
+                }
+    
+                case value:{
+                    console.log("Maior de idade");
+                }
+                        
+            }
+        }*/
+        
+    //1 -Um passo é equivalente a 0,82 metros. Crie um programa que leia uma distância em quilômetros e calcule e exiba quanto é essa distância em passos
+    //Console
+
+    //Qual a distância em KM?Distância em KM = 3030 km é equivalente a 36585 passos.
+    
+        const VALOR_UM_PASSO = 0.82;
+                
+        function converterPassosEmKM (){
+            var KmInformado = prompt("Qual a distancia dem KM ?");
+            console.log("Qual a distancia dem KM ?");
+            console.log("Distancia em KM:" + KmInformado);
+            var quantidadedePassos = (KmInformado * 1000) / VALOR_UM_PASSO;
+            console.log(KmInformado + "Km é equivalente a " + quantidadedePassos, "passos");
+        
+        }
+
+        //2 -Na construção civil, sabe-se que 8 pedreiros levam 72 horas para construir um muro. 
+        //Faça um programa que leia a quantidade de pedreiros disponíveis e informe quantas horas eles demorarão para terminar o muro.
+
+        //Console
+        
+        //Qual pedreiros estão disponíveis? Os 2 pedreiros levam 288 horas para terminar o muro.
+
+        const BASE_HORA = 72;
+        const BASE_PEDREIRO = 8;
+
+        function converterTempoPedreiros (){
+            var pedreirosDisponiveis = prompt ("Quantos pedreiro estão disponiveis?");
+            console.log("Quantos pedreiro estão disponiveis?");
+            console.log("pedreiro disponiveis:" + pedreirosDisponiveis);
+            var resultado = (BASE_HORA * BASE_PEDREIRO) / pedreirosDisponiveis;
+
+            if (pedreirosDisponiveis > 2) {
+                console.log("Os ",pedreirosDisponiveis," pedreiros levam ",resultado," horas para terminar o muro.");
+
+            } else {
+                console.log(" O pedreiro leva ",resultado," horas para terminar o muro.");
+            }
+        }
+
+        //3 -Escreva um programa que leia duas notas de um aluno, calcule e exiba a média que ele obteve e informe se o aluno foi aprovado ou não na disciplina.
+
+        //Console
+        
+        //Qual sua primeira nota?
+        //Qual sua segunda nota?
+        //Sua média foi 7,5.Parabéns você foi aprovado.
+        
+        
+        
+        
+
+        function CalcularMediaAluno (){
+            var nota1 = prompt ("Digite a primeira nota: ");
+            console.log("Digite a primeira nota: ");
+            console.log("Nota Aluno 1: "  + nota1);
+
+            var nota2 = prompt ("Digite a segunda nota: ");
+            console.log("Digite a segunda nota: ");
+            console.log("Nota aluno 2: "  + nota2);
+
+            var resultadoMedia = (nota1 + nota2) / 2;
+
+            if (resultadoMedia => 60) {
+                console.log("Sua média foi",resultadoMedia,"você foi aprovado");
+            } 
+            
+            else if(resultadoMedia = 60){
+                console.log("Sua média foi",resultadoMedia,"você está de recuperação");
+            }
+
+            else {
+                console.log("Sua média foi",resultadoMedia,"você está reprovado");
+            }
+        }
+
+          //4 -Crie um programa que leia o nome de uma pessoa e seu sexo (m para masculino e f para feminino). Ao final, o programa deve imprimir a mensagem
+         //“Bom dia senhor” ou “Bom dia senhora” seguida do nome.
+
+         //Console
+         //Qual seu nome?
+         //José
+         //Qual seu sexo?
+         //Bom dia, senhor José
+
+
+         function lerSexo (){
+            var nome = prompt ("Qual seu nome ? ");
+            console.log("Qual seu nome ? ");
+            console.log("nome: "  + nome);
+
+            var sexo = prompt ("Qual seu sexo ? ");
+            console.log("Qual seu sexo ? ");
+            console.log("sexo: "  + nome);
+
+            var masculino, feminino;
+            
+            if (sexo = masculino = feminino) {
+                console.log("Bom dia senhor");
+                
+            } else {
+                console.log("Bom dia senhora");
+            }
+
+
+         }
+
